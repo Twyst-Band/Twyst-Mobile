b5e8a32e10663176ac60504a1c7a619def7bc993
karo <cazacuchristian@gmail.com>
2025-10-04 21:28:32 +0300

    Initial Commit

---
+++ Twyst/TwystApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwystApp.swift
+//  Twyst
+//
+//  Created by Karo on 04.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TwystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
